ce2553a0508e7a4311445f0585ea43f3937984ab
Juniee5 <96186923+Juniee5@users.noreply.github.com>
2022-04-13 18:16:37 +0700

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -10,7 +10,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
+        fields = ('__all__')
         read_only_fields = ('post',)
 
 
@@ -28,4 +28,4 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'author', 'group', 'pub_date')
+        fields = ('__all__')
