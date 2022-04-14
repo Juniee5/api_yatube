32aedfd117ba81d129d34411d50cff5a8e518b62
Juniee5 <96186923+Juniee5@users.noreply.github.com>
2022-04-14 15:36:28 +0700

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,6 +12,6 @@ router_v1.register(r'posts/(?P<post_id>\d+)/comments',
                    CommentViewSet, basename='comments')
 
 urlpatterns = [
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/api-token-auth/', views.obtain_auth_token, name='api_token'),
     path('v1/', include(router_v1.urls)),
 ]
