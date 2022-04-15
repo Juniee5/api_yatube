3728e30b7f6ece9109203d9f85bb595f3b590cb4
Juniee5 <96186923+Juniee5@users.noreply.github.com>
2022-04-15 14:13:42 +0700

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,4 +4,4 @@ from rest_framework import permissions
 class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == r.user or r.method in permissions.SAFE_METHODS
+        return obj.author == request.user or request.method in permissions.SAFE_METHODS
